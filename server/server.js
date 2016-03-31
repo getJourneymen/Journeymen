369c6ac143dc3d0628953eba01af9bd08384f0bf
Jay McBee <jmcbee1@gmail.com>
2016-03-31 15:24:54 -0500

    oauth functioning

--- server/server.js
+++ server/server.js
@@ -1,55 +1,20 @@
 var express = require('express');
 var passport = require('passport');
 var bodyParser = require('body-parser');
-var SoundCloudStrategy = require('passport-soundcloud').Strategy;
 var path = require('Path');
 var logout = require('express-passport-logout')
-var auth = require('auth');
+var auth = require('./auth');
 //var db = require('db');
 
 
 
 
 //This is necessary to allow persistent login sessions--'remember me'
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-passport.use(new SoundCloudStrategy({
-    clientID: auth.soundCloud_id;
-    clientSecret:auth.soundCloud_secret,
-    callbackURL: "http://localhost/auth/soundcloud/callback"
-  },
-    function(accessToken, refreshToken, profile, done) {
-    //database function to find user record or create and store record
-      User.findOrCreate({ oauthID: profile.id }, function(err, user) {
-        if(err) console.log(err); 
-        if (!err && user != null) done(null, user);
-        else {
-          var user = new User()
-         //create user profile and stash in database;
-       }
-      return done(null,user);
-    });
-  }));
 
 var app = express();
 
 
-app.use(express.cookieParser());
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'kitkat' }));
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + './client'));
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -58,16 +23,12 @@ next();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded ({extended:true}));
 
+var configAuth = require('./passport')(app,express);
+
 /********************************
         app endpoint routes
 *********************************/
-app.get('/login',function(req,res){
-  passport.authenticate('soundcloud'),
-   function(req, res){
-    // The request will be redirected to SoundCloud for authentication, so this
-    // function will not be called.
-  }
-})
+app.get('/login', passport.authenticate('soundcloud'));
 
 
 app.get('/',function(req,res){
@@ -106,7 +67,7 @@ app.get('/logout',function(req,res){
 ***********************************/
 
 app.get('/auth/soundcloud/callback',
-  passport.authenticate('soundcloud', { failureRedirect: '/login' }),
+  passport.authenticate('soundcloud', { failureRedirect: '/login', successRedirect: '/' }),
   function(req, res){
     //stash res token in session db.
     res.redirect('/');
