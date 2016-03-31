fc89286f907fd73c0e519b4831624fec6b2226c5
MKS-students <mks.students@hackreactor.com>
2016-03-31 11:01:24 -0500

    add knex and db file

---
+++ server/db.js
@@ -0,0 +1,8 @@
+//These three requires give us access to the knex library and create the database connection
+var config      = require('../knexfile.js');
+var env         = 'development';
+var knex        = require('knex')(config[env]);
+
+module.exports = knex;
+
+knex.migrate.latest([config]);
