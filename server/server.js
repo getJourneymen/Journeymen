8f666d52eb1fecb02243965896d8e848da99d8e2
Jay McBee <jmcbee1@gmail.com>
2016-03-31 12:55:55 -0500

    server is up --still missing somedb utilities

--- server/server.js
+++ server/server.js
@@ -1,26 +1,23 @@
 var express = require('express');
-var http = require('http-request');
 var passport = require('passport');
 var bodyParser = require('body-parser');
 var SoundCloudStrategy = require('passport-soundcloud').Strategy;
 var path = require('Path');
 var logout = require('express-passport-logout')
-
-//npm install express-passport-logout --save
-//
+//var db = require('db');
 
 
 var SOUNDCLOUD_CLIENT_ID ='c9eae4a12b0b429d520addb31b57046f';
 var SOUNDCLOUD_CLIENT_SECRET ='8e5da229efe9fed0f350759cb41caab4';
 
-//This is necessary to allow persistent login sessions--
-// passport.serializeUser(function(user, done) {
-//   done(null, user);
-// });
+//This is necessary to allow persistent login sessions--'remember me'
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
-// passport.deserializeUser(function(obj, done) {
-//   done(null, obj);
-// });
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 passport.use(new SoundCloudStrategy({
     clientID: SOUNDCLOUD_CLIENT_ID,
@@ -29,40 +26,53 @@ passport.use(new SoundCloudStrategy({
   },
     function(accessToken, refreshToken, profile, done) {
     //database function to find user record or create and store record
-      User.findOne({ oauthID: profile.id }, function(err, user) {
+      User.findOrCreate({ oauthID: profile.id }, function(err, user) {
         if(err) console.log(err); 
         if (!err && user != null) done(null, user);
         else {
+          var user = new User()
          //create user profile and stash in database;
        }
-      return done(null, profile);
+      return done(null,user);
     });
   }));
 
 var app = express();
 
 
-app.configure(function() {
-  app.use(express.cookieParser());
-  app.use(express.methodOverride());
-  app.use(express.session({ secret: 'kitkat' }));
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/client'));
-  app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-  });
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded ({extended:true}));
-})  
+// // app.use(express.cookieParser());
+// // app.use(express.methodOverride());
+//app.use(express.session({ secret: 'kitkat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/client'));
+app.use(function(req, res, next) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded ({extended:true}));
+
+/********************************
+        app endpoint routes
+*********************************/
+app.get('/login',function(req,res){
+  passport.authenticate('soundcloud'),
+   function(req, res){
+    // The request will be redirected to SoundCloud for authentication, so this
+    // function will not be called.
+  }
+})
+
 
 app.get('/',function(req,res){
-  res.sendFile('/index.html')
+  //res.sendFile('/index.html')
+  res.send('you did it');
 })
 
 /*
@@ -91,23 +101,21 @@ app.get('/logout',function(req,res){
   res.redirect('/');
 })
 
-
-//Auth routes
-
-app.get('/auth/soundcloud',
-  passport.authenticate('soundcloud'),
-  function(req, res){
-    // The request will be redirected to SoundCloud for authentication, so this
-    // function will not be called.
-  });
+/***********************************
+          Auth routes
+***********************************/
 
 app.get('/auth/soundcloud/callback',
   passport.authenticate('soundcloud', { failureRedirect: '/login' }),
-  function(req, res) {
+  function(req, res){
+    //stash res token in session db.
     res.redirect('/');
   });
 
-//middleware auth check
+/**********************************
+        middleware auth check
+***********************************/
+
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
