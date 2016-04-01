050940c0bceefbd1b1e29220263bd69cb1086032
MKS-students <mks.students@hackreactor.com>
2016-04-01 15:13:49 -0500

    static files paths work

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ var util = require('./utilities.js');
 var app = express();
 
 
-app.use(express.static('./client'));
+app.use(express.static(path(__dirname+'/../client'));
 app.use(function(req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -35,7 +35,7 @@ app.get('/login', passport.authenticate('soundcloud'));
   3. Logout
 ******************************************/
 app.get('/',function(req,res){
-  res.sendFile('/index.html')
+  res.sendFile(path.resolve(__dirname+'/../client/index.html'));
 })
 app.get('/search', function(req,res){
   util.searchUsers(req.body.query)
