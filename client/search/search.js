c24cefb3a4ede0de3644e8afab576c5f49a6a08f
Jake Lambert <jake@onahill.co>
2016-04-01 15:52:06 -0500

    Search service initial commit

--- client/search/search.js
+++ client/search/search.js
@@ -8,4 +8,26 @@ app.controller('ResultsCtlr', ['$scope', 'SearchSvc', function($scope, SearchSvc
 
 app.factory('SearchSvc', ['$http', function($http) {
 
+  var factoryObj = {};
+  var results = {};
+
+  var searchUri = '/search';
+
+  factoryObj.data = function() {
+    return results;
+  }
+
+  factoryObj.search = function(params) {
+    //Params should be an object which may contain any of the following keys:
+    //start, end, instrument, location
+    $http.get(searchUri, {params: params })
+      .then(function(data){
+        results = data; //What is the result data going to look like??
+      })
+      .catch(function(err){
+        console.log("Error retreiving data", err);
+      })
+  }
+  return factoryObj;
+
 }]);
