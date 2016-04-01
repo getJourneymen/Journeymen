ff20659d2f73ec368d6aaf2f94874111a276c9d8
Jake Lambert <jake@onahill.co>
2016-04-01 15:29:18 -0500

    Controller and factory definitions

---
+++ client/search/search.js
@@ -0,0 +1,11 @@
+app.controller('SearchCtlr', ['$scope', 'SearchSvc', function($scope, SearchSvc) {
+
+}]);
+
+app.controller('ResultsCtlr', ['$scope', 'SearchSvc', function($scope, SearchSvc) {
+
+}]);
+
+app.factory('SearchSvc', ['$http', function($http) {
+
+}]);
