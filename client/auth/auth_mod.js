4acf5e039c8ce0f867e69038d46dceb9695dbb91
Jake Lambert <jake@onahill.co>
2016-04-04 14:52:10 -0500

    authorization module

---
+++ client/auth/auth_mod.js
@@ -0,0 +1,16 @@
+app.factory('AuthSvc', [function() {
+
+  var authToken = null;
+
+  var auth = {};
+
+  auth.retrieveToken = function() {
+    return authToken;
+  }
+
+  auth.setToken = function(token) {
+    authToken = token;
+  }
+
+  return auth;
+}]);
