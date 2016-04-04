be7fa1fa308d85a8fc87f13d0a813e0b2f4a1506
Feben Teklemicael <f.teklemicael@gmail.com>
2016-04-04 14:48:06 -0500

    Change callbacks into promises. Add state transition.

--- client/user/setavail.js
+++ client/user/setavail.js
@@ -1,42 +1,36 @@
 angular.module('JourneymenApp')
-    .controller('AvailCtlr', function($scope, $state, 'AvailSvc') {
+    .controller('AvailCtlr', function($scope, $state, 'AvailSvc', 'AuthSvc') {
         $scope.start = '';
         $scope.end = '';
         $scope.instrument = {}; //What is the return format from the form checkboxes?
 
         $scope.setAvailability = function() {
             AvailSvc.setAvail({
-                time: {
-                    journeymen_id: AvailSvc.retrieveID(), //timing?
-                    start: $scope.start,
-                    end: $scope.end,
-                    instrument: $scope.instrument
-                }
-            })
-            //.then ? //handle state transition to $state.go('profile')
-
+                    time: {
+                        authToken: AuthSvc.retrieveToken(),
+                        start: $scope.start,
+                        end: $scope.end,
+                        instrument: $scope.instrument
+                    }
+                })
+                .then(function(res) {
+                    console.log('Successfully set availibility: ', res.statusText);
+                    $state.go('profile');
+                })
+                .catch(function(err) {
+                    console.log('Error setting availibility: ', err.statusText);
+                });
         }
-
     })
     .factory('AvailSvc', function($http) {
-
-        function retrieveID() {
-            //retriev ID from local storage?
-        }
 
         var setAvailUri = '/avail';
 
         function setAvail(availData) {
-            $http.post(setAvailUri, availData)
-                .then(function successCB(res) {
-                    console.log('Successfully set availibility: ', res.statusText)
-                }, function errorCB(err) {
-                    console.log('Error setting availibility: ', err.statusText)
-                });
-
+            return $http.post(setAvailUri, availData);
         }
+
         return {
-            retrieveID: retrieveID,
             setAvail: setAvail
         }
     });
