1dcf668b483ed66dea7006f88343c478fddaff08
Feben Teklemicael <f.teklemicael@gmail.com>
2016-04-04 13:53:14 -0500

    Refactor setAvail Ctrl and Svc Refactor $http error handeling using succes and error callbacks Change html to checkbox input (content to be generated from insturments table later)

--- client/user/setavail.js
+++ client/user/setavail.js
@@ -1,23 +1,42 @@
 angular.module('JourneymenApp')
-    .controller('AvailCtlr', function($scope,$state, 'AvailSvc') {
+    .controller('AvailCtlr', function($scope, $state, 'AvailSvc') {
+        $scope.start = '';
+        $scope.end = '';
+        $scope.instrument = {}; //What is the return format from the form checkboxes?
 
+        $scope.setAvailability = function() {
+            AvailSvc.setAvail({
+                time: {
+                    journeymen_id: AvailSvc.retrieveID(), //timing?
+                    start: $scope.start,
+                    end: $scope.end,
+                    instrument: $scope.instrument
+                }
+            })
+            //.then ? //handle state transition to $state.go('profile')
+
+        }
 
     })
     .factory('AvailSvc', function($http) {
+
+        function retrieveID() {
+            //retriev ID from local storage?
+        }
 
         var setAvailUri = '/avail';
 
         function setAvail(availData) {
             $http.post(setAvailUri, availData)
-                .then(function(res) {
-                    console.log('Successfully set availibility: ', res)
-                })
-                .catch(function(err) {
-                    console.log('Error setting availibility: ', err)
-                })
+                .then(function successCB(res) {
+                    console.log('Successfully set availibility: ', res.statusText)
+                }, function errorCB(err) {
+                    console.log('Error setting availibility: ', err.statusText)
+                });
+
         }
         return {
-
+            retrieveID: retrieveID,
             setAvail: setAvail
         }
     });
