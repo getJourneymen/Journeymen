e93275bb5303e574c0825cb02f90f023eb531d4c
Jake Lambert <jake@onahill.co>
2016-04-07 18:34:57 -0500

    add http interceptor

--- client/app/app.js
+++ client/app/app.js
@@ -46,8 +46,29 @@ angular.module('JourneymenApp', [
             controller: 'AvailCtlr',
             authenticate: true
         })
-    $urlRouterProvider.otherwise("/signin");
-});
+    $urlRouterProvider.otherwise("/search");
+})
+.factory('authHttpResponseInterceptor',['$q','$location',function($q,$location){
+    return {
+        response: function(response){
+            if (response.status === 401) {
+                console.log("Response 401");
+            }
+            return response || $q.when(response);
+        },
+        responseError: function(rejection) {
+            if (rejection.status === 401) {
+                console.log("Response Error 401",rejection);
+                $location.path('/signin').search('returnTo', $location.path());
+            }
+            return $q.reject(rejection);
+        }
+    }
+}])
+.config(['$httpProvider',function($httpProvider) {
+    //Http Intercpetor to check auth failures for xhr requests
+    $httpProvider.interceptors.push('authHttpResponseInterceptor');
+}]);
 // //prevent transition if not authenticated
 // .run(function($rootScope, $state, AuthService){
 // 	$rootScope.$on('$stateChangeStart', function(event,toState, toParams,fromState,fromParams){
