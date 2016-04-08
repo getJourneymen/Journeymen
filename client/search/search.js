6c1be97b70c01d0b908531b0bc1c657ac4659912
Jay McBee <jmcbee1@gmail.com>
2016-04-07 23:07:41 -0500

    queries working again

--- client/search/search.js
+++ client/search/search.js
@@ -21,6 +21,9 @@ angular.module('JourneymenApp.Search',[])
 ])
 .controller('ResultsCtlr', ['$scope', 'SearchSvc', function($scope, SearchSvc) {
 
+
+app.controller('ResultsCtlr', ['$scope', 'SearchSvc', function($scope, SearchSvc) {
+
   $scope.results = {}
 
   SearchSvc.data().then(function(data) {
