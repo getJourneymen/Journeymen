ed6956c9921bfac8afcfb03a989dfcfd5f154c8e
Jake Lambert <jake@onahill.co>
2016-04-11 11:24:07 -0500

    end date widgets for setavail

--- client/user/setavail.js
+++ client/user/setavail.js
@@ -1,24 +1,17 @@
 angular.module('JourneymenApp.Avail',['JourneymenApp.Instruments'])
 .controller('AvailCtlr', ['$scope', '$state', 'AvailSvc', 'InstrSvc', function($scope, $state, AvailSvc, InstrSvc) {
-        $scope.start = '';
-        $scope.end = '';
         $scope.instruments = InstrSvc.getInstruments();
         $scope.selectedInstrument = null;
         $scope.startDate = null;
         $scope.startTime = null;
+        $scope.endDate = null;
+        $scope.endTime = null;
 
         $scope.setAvailability = function() {
 
-          var dd = new Date($scope.startDate).getDate();
-          var mm = new Date($scope.startDate).getMonth()+1;
-          var yy = new Date($scope.startDate).getFullYear();
-          var hh = new Date($scope.startTime).getHours();
-          var ms = new Date($scope.startTime).getMinutes();
-          var startDate = mm + '/' + dd + '/' + yy + ' ' + hh + ':' + ms + ":00";
-
             AvailSvc.setAvail({
                         start: combineDateTime($scope.startDate, $scope.startTime),
-                        end: $scope.end,
+                        end: combineDateTime($scope.endDate,$scope.endTime),
                         instrument: $scope.selectedInstrument
                 })
                 .then(function(res) {
