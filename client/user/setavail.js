d3da48df9d5c1e0bf5f2d3de591495fc4d48e9cf
Jake Lambert <jake@onahill.co>
2016-04-11 11:07:20 -0500

    set avail start

--- client/user/setavail.js
+++ client/user/setavail.js
@@ -4,10 +4,20 @@ angular.module('JourneymenApp.Avail',['JourneymenApp.Instruments'])
         $scope.end = '';
         $scope.instruments = InstrSvc.getInstruments();
         $scope.selectedInstrument = null;
+        $scope.startDate = null;
+        $scope.startTime = null;
 
         $scope.setAvailability = function() {
+
+          var dd = new Date($scope.startDate).getDate();
+          var mm = new Date($scope.startDate).getMonth()+1;
+          var yy = new Date($scope.startDate).getFullYear();
+          var hh = new Date($scope.startTime).getHours();
+          var ms = new Date($scope.startTime).getMinutes();
+          var startDate = mm + '/' + dd + '/' + yy + ' ' + hh + ':' + ms + ":00";
+
             AvailSvc.setAvail({
-                        start: $scope.start,
+                        start: combineDateTime($scope.startDate, $scope.startTime),
                         end: $scope.end,
                         instrument: $scope.selectedInstrument
                 })
@@ -18,6 +28,15 @@ angular.module('JourneymenApp.Avail',['JourneymenApp.Instruments'])
                 .catch(function(err) {
                     console.log('Error setting availibility: ', err.statusText);
                 });
+
+              function combineDateTime(date, time) {
+                var dd = new Date(date).getDate();
+                var mm = new Date(date).getMonth()+1;
+                var yy = new Date(date).getFullYear();
+                var hh = new Date(time).getHours();
+                var ms = new Date(time).getMinutes();
+                return mm + '/' + dd + '/' + yy + ' ' + hh + ':' + ms + ":00";
+              }
         }
     }])
 .factory('AvailSvc', function($state,$http) {
